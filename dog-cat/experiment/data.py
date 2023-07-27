f7c947ad98abe398a71f5780fe22d9cbf9aaf582
Denis Abrantes <denis.abrantes@Deniss-MacBook-Pro.local>
2023-07-27 12:49:33 -0500

    changed pachd auth

--- dog-cat/experiment/data.py
+++ dog-cat/experiment/data.py
@@ -44,7 +44,8 @@ def download_pach_repo(pachyderm_host, pachyderm_port, repo, branch, root, token
     if not os.path.exists(root):
         os.makedirs(root)
 
-    client = python_pachyderm.Client(host=pachyderm_host, port=pachyderm_port, auth_token=token)
+    #client = python_pachyderm.Client(host=pachyderm_host, port=pachyderm_port, auth_token=token)
+    client = python_pachyderm.Client(host=pachyderm_host, port=pachyderm_port)
     files = []
 
     for diff in client.diff_file((repo, branch), "/"):
