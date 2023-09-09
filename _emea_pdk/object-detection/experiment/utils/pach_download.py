03f97e379c25fd5e7848eab07e54597c08e3116c
Denis Abrantes <denis.abrantes@hpe.com>
2023-09-09 12:43:08 -0500

    obj_detection

--- _emea_pdk/object-detection/experiment/utils/pach_download.py
+++ _emea_pdk/object-detection/experiment/utils/pach_download.py
@@ -2,10 +2,8 @@
 import os
 import shutil
 
-import python_pachyderm
-
-from python_pachyderm.pfs import Commit
-from python_pachyderm.proto.v2.pfs.pfs_pb2 import FileType
+import pachyderm_sdk
+from pachyderm_sdk.api.pfs import File, FileType
 
 
 def safe_open_wb(path):
@@ -26,19 +24,19 @@ def get_pach_repo_folder(
 
     print(f"Starting to download dataset: {repo}@{branch}")
 
-    client = python_pachyderm.Client(
+    client = pachyderm_sdk.Client(
         host=pachyderm_host, port=pachyderm_port, auth_token=token
     )
 
-    for file_info in client.walk_file(
-            Commit(repo=repo, id=branch, project=project), "/"):
-            src_path = file_info.file.path
+    for file_info in client.pfs.walk_file(file=File.from_uri(f"{project}/{repo}@{branch}")):
+        src_path = file_info.file.path
+        #print(f"Got src='{src_path}'")
 
-            if file_info.file_type != FileType.FILE:
-                if src_path != "/":
-                    folder_name  = src_path
-                    break;
-    print("Repository Folder: ", folder_name)
+        if file_info.file_type != FileType.FILE:
+            if src_path != "/":
+                folder_name  = src_path
+                break;
+    print("Using Folder: ", folder_name)
     return folder_name.replace("/","")
 
 
@@ -61,21 +59,15 @@ def download_full_pach_repo(
         host=pachyderm_host, port=pachyderm_port, auth_token=token
     )
 
-    for file_info in client.walk_file(
-            Commit(repo=repo, id=branch, project=project), "/"):
-            src_path = file_info.file.path
-            des_path = os.path.join(root, src_path[1:])
-            print(f"Saving File: '{des_path}'")
+    for file_info in client.pfs.walk_file(file=File.from_uri(f"{project}/{repo}@{branch}")):
+        src_path = file_info.file.path
+        des_path = os.path.join(root, src_path[1:])
+        print(f"Saving File: '{des_path}'")
 
-            if file_info.file_type == FileType.FILE:
-                if src_path != "":
-                    # get file
-                    src_file = client.get_file(
-                        Commit(repo=repo, id=branch, project=project), src_path
-                    )
-                    # copy file to folder
-                    with safe_open_wb(des_path) as dest_file:
-                        shutil.copyfileobj(src_file, dest_file)
-                    # files.append((src_path, des_path))
+        if file_info.file_type == FileType.FILE:
+            if src_path != "":
+                src_file = client.pfs.pfs_file(file=File.from_uri(f"{project}/{repo}@{branch}:{src_path}"))
+                with safe_open_wb(des_path) as dest_file:
+                    shutil.copyfileobj(src_file, dest_file)
     print("Download operation ended")
     return root
